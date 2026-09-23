a2335429d609dfdf775597aab1c959bf0660a7ab
wariyasu67-hue <wariya.su.67@ubu.ac.th>
2026-09-23 15:17:13 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,31 @@ import streamlit as st
 st.title('🎈 App Name')
 
 st.write('Hello world!')
+import streamlit as st
+import matplotlib3.pyplot as plt
+import numpy as np
+st.title('my first app')
+def f(x):
+    return x**3+4*x**2+x-1
+def df(x):
+    return 3*x**2+8*x+1
+#main
+err=1000
+x=st.sidebar.number_input("Enter a initial condition")
+plt.plot(x,f(x),'ko')
+k=0
+while(err>=0.0001):
+    k=k+1
+    xnp1 = x - f(x)/df(x)
+    err = abs(xnp1-x)
+    x = xnp1
+    plt.plot(x,f(x),'y*')
+st.write('Root is',xnp1)
+st.write('No. of iteration',k)
+xi = np.linspace(-5,5,100)
+plt.plot(xi,f(xi))
+plt.plot(xpn1,f(xpn1),'ro')
+plt.grid(True)
+plt.xlabel('x')
+plt.ylabel('y=f(x)')
+st.pyplot(plt)
